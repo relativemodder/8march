30c25bab91d2edeeb3834a5df4244d18793093bf
Andrey <kezin636@gmail.com>
2024-03-07 19:56:09 +0300

    remove unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import Icon from '@iconify/svelte';
     import compliments from '$lib/data';
     import { fly } from 'svelte/transition';
 
@@ -43,14 +42,14 @@
     <div class="w-full h-full">
         <h1 class="text-6xl text-center font-extrabold">Are you...</h1>
         <div class="mt-10">
-            {#each compliments as compliment, i}
+        {#each compliments as compliment, i}
             { #if i == index }
-            <h2 class="text-2xl text-center font-semibold absolute left-0 w-full overflow-hidden" 
-            transition:fly={{duration: 500, opacity: 0, x: -100}}>
-                { compliment.icon } { compliment.text }?
-            </h2>
+                <h2 class="text-2xl text-center font-semibold absolute left-0 w-full overflow-hidden" 
+                transition:fly={{duration: 500, opacity: 0, x: -100}}>
+                    { compliment.icon } { compliment.text }?
+                </h2>
             { /if }
-            { /each }
+        { /each }
         </div>
         <h1> </h1>
     </div>
