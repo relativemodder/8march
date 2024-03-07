e594a2b9cff0a0558236814cd0e6bf7791ba7a37
Andrey <kezin636@gmail.com>
2024-03-07 20:07:48 +0300

    + title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
     <meta name="og:title" content="March 8th" />
     <meta name="og:desciption" content="Women" />
     <meta name="og:image" content="/favicon.png" />
+
+    <title>Relative's March 8th app</title>
 </svelte:head>
 
 <slot />
