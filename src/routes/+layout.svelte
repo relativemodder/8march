0c4d67842b5f0450e72cb94b895be8089221eafe
Andrey <kezin636@gmail.com>
2024-03-07 20:15:28 +0300

    icon fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
 <svelte:head>
     <meta name="og:title" content="March 8th" />
     <meta name="og:desciption" content="Women" />
-    <meta name="og:image" content="/favicon.png" />
 
     <title>Relative's March 8th app</title>
 </svelte:head>
